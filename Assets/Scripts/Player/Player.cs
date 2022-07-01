15d8f0b5a02107ff185e7b96966b2669f2ae0cb6
PitoYaga <efe.canoglu@hotmail.com>
2022-07-01 21:49:56 +0300

    last2

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -93,7 +93,7 @@ public class Player : MonoBehaviour
         }
         else
         {
-            SceneManager.LoadScene("DeathScene");
+            StartCoroutine("LoadDeathScene");
         }
     }
     
@@ -389,7 +389,7 @@ public class Player : MonoBehaviour
     
     IEnumerator LoadDeathScene()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene("DeathScene");
     }
     
